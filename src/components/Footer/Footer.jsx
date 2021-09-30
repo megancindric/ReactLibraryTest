9ffe63652d73dfa80cf4628b2865edc0407afabd
Megan Cindric <megancindric@gmail.com>
2021-09-30 16:43:09 -0500

    Adding a footer component w/ css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer(props){
+    return(
+            <div className="footer">
+                <div className="text-center p-3">
+                (a little bit of everything, all of the time)
+                </div>
+            </div>
+    )
+}
+
+export default Footer;
